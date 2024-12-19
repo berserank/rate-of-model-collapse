6e08b7873c2705f6246ce98c435f58ebed27d4d5
Aditya Nanda Kishore <92176497+berserank@users.noreply.github.com>
2024-12-19 11:11:49 +0530

    Add files via upload

--- ngram_sim.sh
+++ ngram_sim.sh
@@ -1,11 +1,11 @@
-python ngram_sim.py --input_file ngram_sim/bigram/train_sampled.txt --output_file ngram_sim/bigram/generation1.txt --n 2
-python ngram_sim.py --input_file ngram_sim/bigram/generation1.txt --output_file ngram_sim/bigram/generation2.txt --n 2
-python ngram_sim.py --input_file ngram_sim/bigram/generation2.txt --output_file ngram_sim/bigram/generation3.txt --n 2
-python ngram_sim.py --input_file ngram_sim/bigram/generation3.txt --output_file ngram_sim/bigram/generation4.txt --n 2
-python ngram_sim.py --input_file ngram_sim/bigram/generation4.txt --output_file ngram_sim/bigram/generation5.txt --n 2
-python ngram_sim.py --input_file ngram_sim/bigram/generation5.txt --output_file ngram_sim/bigram/generation6.txt --n 2
-python ngram_sim.py --input_file ngram_sim/bigram/generation6.txt --output_file ngram_sim/bigram/generation7.txt --n 2
-python ngram_sim.py --input_file ngram_sim/bigram/generation7.txt --output_file ngram_sim/bigram/generation8.txt --n 2
-python ngram_sim.py --input_file ngram_sim/bigram/generation8.txt --output_file ngram_sim/bigram/generation9.txt --n 2
-python ngram_sim.py --input_file ngram_sim/bigram/generation9.txt --output_file ngram_sim/bigram/generation10.txt --n 2
-
+mkdir tmp
+python ngram_sim.py --input_file train_sampled.txt --output_file tmp/generation1.txt --n 2
+python ngram_sim.py --input_file tmp/generation1.txt --output_file tmp/generation2.txt --n 2
+python ngram_sim.py --input_file tmp/generation2.txt --output_file tmp/generation3.txt --n 2
+python ngram_sim.py --input_file tmp/generation3.txt --output_file tmp/generation4.txt --n 2
+python ngram_sim.py --input_file tmp/generation4.txt --output_file tmp/generation5.txt --n 2
+python ngram_sim.py --input_file tmp/generation5.txt --output_file tmp/generation6.txt --n 2
+python ngram_sim.py --input_file tmp/generation6.txt --output_file tmp/generation7.txt --n 2
+python ngram_sim.py --input_file tmp/generation7.txt --output_file tmp/generation8.txt --n 2
+python ngram_sim.py --input_file tmp/generation8.txt --output_file tmp/generation9.txt --n 2
+python ngram_sim.py --input_file tmp/generation9.txt --output_file tmp/generation10.txt --n 2
